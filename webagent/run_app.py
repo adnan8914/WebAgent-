55986ba197e0b56fd43d44f3c2e60be262d4a439
MD ADNAN <76099117+adnan8914@users.noreply.github.com>
2025-04-24 20:18:19 +0530

    Update run_app.py

--- webagent/run_app.py
+++ webagent/run_app.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+
+# 🔧 Patch sqlite3 to use the pysqlite3-binary version
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from webagent.main import main
  
 if __name__ == "__main__":
-    main() 
+    main()
